9858441e3dd3bf8b892068ff88641310a9a8709f
Jimmy Jenkins <mchorselessrider@gmail.com>
2017-01-02 17:16:20 +0000

    Bump version

--- src/main/java/mchorse/imaginary/Imaginary.java
+++ src/main/java/mchorse/imaginary/Imaginary.java
@@ -15,11 +15,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
  * 
  * Animated GIFs are planned in the future.
  */
-@Mod(modid = Imaginary.MODID, version = Imaginary.VERSION)
+@Mod(name = Imaginary.MODNAME, modid = Imaginary.MODID, version = Imaginary.VERSION)
 public class Imaginary
 {
+    public static final String MODNAME = "Imaginary";
     public static final String MODID = "imaginary";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
 
     public static final String CLIENT_PROXY = "mchorse.imaginary.ClientProxy";
     public static final String SERVER_PROXY = "mchorse.imaginary.CommonProxy";
